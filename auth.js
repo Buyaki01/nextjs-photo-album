6889cbb02559ebffc0c9b0f11892d82c84ade479
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-15 16:33:52 +0300

    Set up MongoDBAdapter

--- auth.js
+++ auth.js
@@ -1,9 +1,15 @@
 import NextAuth from "next-auth"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "./lib/mongodb"
 import Google from "next-auth/providers/google"
+import authConfig from "./auth.config"
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
   providers: [Google],
+  adapter: MongoDBAdapter(clientPromise),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
