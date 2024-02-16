5eaf3e3a7f636f512bca3d4b1851ce12dbd40d35
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-16 07:33:27 +0300

    update auth middleware with what to do with the routes that do not need authentication, Add routes file to include routes that do not require authentication

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -13,12 +13,15 @@ const LandingPage = async () => {
         <p className="text-gray-700 text-md md:text-lg mb-2">
           Explore and organize your memories with our photo album application.
         </p>
-        <p className="text-gray-700 text-lg mb-6"><span className="font-bold">Ready to get started?</span> Login with your Google account below:</p>
+        <p className="text-gray-700 text-md mb-6">
+          Ready to get started? 
+          <span className="font-bold"> Please login with your Google account below before proceeding to the Home Page</span>
+        </p>
         <div className="flex justify-center">
           <button 
-            className="bg-gray-500 flex items-center justify-center gap-3 rounded-lg w-2/3 text-white px-6 py-3 font-semibold hover:bg-opacity-80"
+            className="flex items-center justify-center gap-2 bg-gray-400 rounded-lg w-2/3 text-white px-4 py-2 font-semibold hover:bg-opacity-80"
           >
-            <FaGoogle />
+            <FaGoogle className="text-lg"/>
             Continue with Google
           </button>
         </div>
--- routes.js
+++ routes.js
@@ -5,3 +5,14 @@
 export const publicRoutes = [
   "/"
 ]
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ */
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * The default redirect path after logging in
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/home"
