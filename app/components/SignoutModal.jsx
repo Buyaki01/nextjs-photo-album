be1881c287274cc8c04a7e05f64de46c1d5fce3d
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-16 12:22:57 +0300

    Add signout modal for user to sign out

---
+++ app/components/SignoutModal.jsx
@@ -0,0 +1,30 @@
+const SignoutModal = ({ isOpen, title, description, confirmText, onCancel, onConfirm }) => {
+  if (!isOpen) {
+    return null
+  }
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75"> 
+      <div className="bg-white p-6 rounded-md w-1/2">
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-gray-700 mb-4">{description}</p>
+        <div className="flex justify-end gap-2">
+          <button
+            className="bg-primary text-white px-4 py-2 rounded-md"
+            onClick={onConfirm}
+          >
+            {confirmText}
+          </button>
+          <button
+            className="bg-gray-700 text-white px-4 py-2 rounded-md"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignoutModal
