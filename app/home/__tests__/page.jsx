60a0e1c24a7c56e076a4f2d6d9a77ff472e405f8
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-20 12:29:43 +0300

    Add test to check whether user name exists in the homepage after fetching data from the API, add mockUsers, mockSetUsers, mockAlbums, mockSetAlbums

--- app/home/__tests__/page.jsx
+++ app/home/__tests__/page.jsx
@@ -1,11 +1,109 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import HomePage from '../page'
+
+const mockUsers = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  }
+]
+
+const mockSetUsers = jest.fn()
+
+const mockAlbums = [
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "qui fuga est a eum"
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "saepe unde necessitatibus rem"
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "distinctio laborum qui"
+  },
+  {
+    "userId": 2,
+    "id": 11,
+    "title": "quam nostrum impedit mollitia quod et dolor"
+  },
+  {
+    "userId": 2,
+    "id": 12,
+    "title": "consequatur autem doloribus natus consectetur"
+  }
+]
+
+const mockSetAlbums = jest.fn()
 
 describe('HomePage', () => {
   it('should render a header with the text "Users"', () => {
     render(<HomePage />)
 
     expect(screen.getByText('Users')).toBeInTheDocument()
+  })
+
+  it('should display loading spinner when loading is true', () => {
+    render(<HomePage loading={true} />)
+    expect(screen.getByTestId('loading-spinner')).toBeInTheDocument()
+  })
+
+  it('should render UsersList when loading is false', async () => {
+    console.log("Testing console log outside loop")
+    render(<HomePage loading={false} users={mockUsers} setUsers={mockSetUsers} albums={mockAlbums} setAlbums={mockSetAlbums} />)
+
+    await waitFor(() => {
+      mockUsers.forEach(async (user) => {
+        const username = await screen.findByTestId(`user-name-${user.id}`)
+        expect(username).toBeInTheDocument()
+      })
+    })
   })
 })
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,13 +5,12 @@ import getUsers from "@/lib/getUsers/getUsers"
 import { useEffect, useState } from "react"
 import toast from "react-hot-toast"
 import Spinner from "../components/Spinner"
-import UsersList from "../components/UsersList"
+import Link from "next/link"
 
 const HomePage = () => {
   const [users, setUsers] = useState([])
   const [albums, setAlbums] = useState([])
   const [loading, setLoading] = useState(true)
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,6 +31,10 @@ const HomePage = () => {
     fetchData()
   }, [])
 
+  const getUserAlbumCount = (userId) => {
+    return albums.filter((album) => album.userId === userId).length
+  }
+
   return (
     <div>
       <h1 className="text-center text-3xl font-bold text-primary">Users</h1>
@@ -44,7 +47,25 @@ const HomePage = () => {
         : (
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 m-3 gap-2 md:gap-6 p-3">
             {users.map(user => (
-              <UsersList key={user.id} user={user} albums={albums} />
+              <Link
+                key={user.id}
+                href={`/user/${user.id}`} 
+                className="border py-3 rounded-lg border-gray-300 flex flex-col items-center"
+              >
+                <div className="flex items-center justify-center bg-pink-500 text-white rounded-full w-16 h-16 mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                  </svg>
+                </div>
+        
+                <h3
+                  data-testid={`user-name-${user.id}`}
+                  className="text-xl truncate w-full text-center px-2 hover:underline hover:text-[#a00a7c]"
+                >
+                  {user.name}
+                </h3>
+                <p>{getUserAlbumCount(user.id)} Albums</p>
+              </Link>
             ))}
           </div>
         )
