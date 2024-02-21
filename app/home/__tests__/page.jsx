89d35a0e688b99b749c29b6d4dfa9239ee87ba7e
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-21 12:54:34 +0300

    Add mockUsers and mockAlbums in the test for the HomePage, Add mockUser and mockUserAlbum in the test for the User page, Change naming of files in the lib folder

--- app/home/__tests__/page.jsx
+++ app/home/__tests__/page.jsx
@@ -3,12 +3,85 @@ import { render, screen, waitFor } from '@testing-library/react'
 import HomePage from '../page'
 import toast from 'react-hot-toast'
 
-jest.mock('@/lib/getUsers', () => jest.fn(() => Promise.resolve(mockUsers)))
-jest.mock('@/lib/getAlbums', () => jest.fn(() => Promise.resolve(mockAlbums)))
+const mockUsers = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  }
+]
 
-jest.mock('react-hot-toast', () => ({
-  error: jest.fn(),
-}))
+const mockAlbums = [
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "qui fuga est a eum"
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "saepe unde necessitatibus rem"
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "distinctio laborum qui"
+  },
+  {
+    "userId": 2,
+    "id": 11,
+    "title": "quam nostrum impedit mollitia quod et dolor"
+  },
+  {
+    "userId": 2,
+    "id": 12,
+    "title": "consequatur autem doloribus natus consectetur"
+  }
+]
+
+jest.mock('../../../lib/getUsers', () => jest.fn(() => Promise.resolve(mockUsers)))
+jest.mock('../../../lib/getAlbums', () => jest.fn(() => Promise.resolve(mockAlbums)))
 
 describe('HomePage', () => {
   it('should render a header with the text "Users"', () => {
@@ -34,16 +107,6 @@ describe('HomePage', () => {
         const userAlbumCountText = screen.getByText(`${userAlbumCount} Albums`)
         expect(userAlbumCountText).toBeInTheDocument()
       })
-    })
-  })
-
-  it('displays an error message if fetching users fails', async () => {
-    jest.mock('@/lib/getUsers', () => jest.fn(() => Promise.reject(new Error('Error fetching users'))))
-  
-    render(<HomePage />)
-  
-    await waitFor(() => {
-      expect(toast.error).toHaveBeenCalledWith('Error fetching users data')
     })
   })
 })
--- app/user/[userId]/__tests__/page.test.jsx
+++ app/user/[userId]/__tests__/page.test.jsx
@@ -2,8 +2,50 @@ import '@testing-library/jest-dom'
 import { render, screen, waitFor } from '@testing-library/react'
 import UserPage from '../page'
 
-jest.mock('@/lib/getUser', () => jest.fn(() => Promise.resolve(mockUser)))
-jest.mock('@/lib/getUserAlbums', () => jest.fn(() => Promise.resolve(mockAlbum)))
+const mockUser = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+
+const mockAlbum = [
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "qui fuga est a eum"
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "saepe unde necessitatibus rem"
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "distinctio laborum qui"
+  }
+]
+
+jest.mock('../../../../lib/getUser', () => jest.fn(() => Promise.resolve(mockUser)))
+jest.mock('../../../../lib/getUserAlbums', () => jest.fn(() => Promise.resolve(mockAlbum)))
 
 jest.mock('next/navigation', () => ({
   ...jest.requireActual('next/navigation'),
