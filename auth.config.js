be4e671d4364877880f994696e15af52bfbe8954
Buyaki01 <rittahbuyaki@gmail.com>
2024-02-28 11:27:18 +0300

    Remove Google, add className hidden for username when on small screen

--- auth.config.js
+++ auth.config.js
@@ -1,4 +1,3 @@
-import Google from "next-auth/providers/google"
 import Github from 'next-auth/providers/github'
 
 export default {
@@ -6,11 +5,6 @@ export default {
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 }
